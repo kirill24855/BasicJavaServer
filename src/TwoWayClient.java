b95521cda30a5e45c1d31f44998e4c278095f1c4
kirill.shpin <kirill.shpin@gmail.com>
2017-06-02 20:41:35 -0700

    Deleted unnecessary files, cleaned up remaining code.

--- src/TwoWayClient.java
+++ src/TwoWayClient.java
@@ -1,100 +1,85 @@
-import TwoWayServer.TwoWayServer;
 import java.io.*;
 import java.net.*;
 
-/*
+/**
  * This Two-Way server and client is based off a simple console chat service.
  * Tested it out and seems to work fine.
  * If you want to see the original code see the attached site.
  * http://way2java.com/networking/chat-program-two-way-communication/
  */
 
-public class TwoWayClient
-{
-	
+public class TwoWayClient {
+
+	private static final String IP = "127.0.0.1";
 	private static final int PORT = 4444;
-	
-	public static void main(String[] args)
-	{
+
+	public static void main(String[] args) {
 		// Tries to connect with server via IP address and port given
 		Socket sock = null;
-		try
-		{
+		try {
 			// Might want to look into seeing how to properly close the socket if something goes wrong.
 			// Never know if a program wants to use the same IP and port.
-			sock = new Socket("127.0.0.1", PORT);
-		}
-		catch (UnknownHostException e)
-		{
+			sock = new Socket(IP, PORT);
+		} catch (UnknownHostException e) {
 			System.err.println("Unknown Host");
 			System.exit(1);
-		}
-		catch (IOException e)
-		{
+		} catch (IOException e) {
 			System.err.println("Could not listen on port: " + PORT);
 			System.exit(1);
 		}
-		
-		
+
 		// Read from keyboard
 		BufferedReader keyRead = new BufferedReader(new InputStreamReader(System.in));
-		
-		// Sending to client
+
+		// Sending to server
 		OutputStream ostream = null;
-		try 
-		{
+		try {
 			ostream = sock.getOutputStream();
-		} 
-		catch (IOException e)
-		{
-			System.err.println("Couldn't send anything to client");
+		} catch (IOException e) {
+			System.err.println("Couldn't send anything to server");
 			System.exit(1);
-		} 
-		
+		}
+
 		PrintWriter pwrite = new PrintWriter(ostream, true);
-		
+
 		// Recieving from server
 		InputStream istream = null;
-		try 
-		{
+		try {
 			istream = sock.getInputStream();
-		}
-		catch (IOException e)
-		{
+		} catch (IOException e) {
 			System.err.println("Could not recieve information from server");
 			System.exit(1);
 		}
-		
+
 		BufferedReader receiveRead = new BufferedReader(new InputStreamReader(istream));
 
 		System.out.println("Start the chat, type and press Enter key");
-		String receiveMessage = null, sendMessage = null;               
+		String receiveMessage;
+		String sendMessage = "";
+
 		/*
 		 * This keeps the connection active by constantly checking to see if any messages
 		 * is being sent by the server. It also sends messages to server.
 		 */
-		while(true)
-		{
+		while (true) {
 			try {
 				sendMessage = keyRead.readLine(); //Keyboard reading
 			} catch (IOException e) {
 				System.err.println("Could not send message");
 				System.exit(1);
 			}
-			
-			pwrite.println(sendMessage);       // Sending to server
-			pwrite.flush();                    // Flush data from memory
-			
+
+			pwrite.println(sendMessage);        // Sending to server
+			pwrite.flush();                     // Flush data from memory
+
 			try {
-				if((receiveMessage = receiveRead.readLine()) != null) //Receive from server
-				{
+				if((receiveMessage = receiveRead.readLine()) != null) { // Receive from server
 					System.out.println(receiveMessage); // Display console
 				}
 			} catch (IOException e) {
-				System.err.println("recieveMessage wasn't initialize");
+				System.err.println("Could not read message from server");
 				System.exit(1);
-			}         
+			}
 		}
 	}
-	
 }
--- src/TwoWayServer/TwoWayServer.java
+++ src/TwoWayServer/TwoWayServer.java
@@ -1,51 +1,50 @@
 package TwoWayServer;
+
 import java.io.*;
 import java.net.*;
 
-/*
+/**
  * This Two-Way server and client is based off a simple console chat service.
  * Tested it out and seems to work fine.
  * If you want to see the original code see the attached site.
  * http://way2java.com/networking/chat-program-two-way-communication/
  */
 
-public class TwoWayServer 
-{
-	
+public class TwoWayServer {
+
 	private static final int PORT = 4444;
-	
-	public static void main(String[] args) throws Exception
-	{
-		ServerSocket sersock = new ServerSocket(PORT);
-		System.out.println("Server  ready for chatting");
-		Socket sock = sersock.accept( );                          
-		
-		//Reads inputs from keyboard
-		BufferedReader keyRead = new BufferedReader(new InputStreamReader(System.in));
-		
+
+	public static void main(String[] args) throws Exception {
+		ServerSocket serSock = new ServerSocket(PORT);
+		System.out.println("Server started");
+		Socket sock = serSock.accept();
+
 		//Sending to client
-		OutputStream ostream = sock.getOutputStream(); 
+		OutputStream ostream = sock.getOutputStream();
 		PrintWriter pwrite = new PrintWriter(ostream, true);
 
-		//Receiving from server
+		//Receiving from client
 		InputStream istream = sock.getInputStream();
 		BufferedReader receiveRead = new BufferedReader(new InputStreamReader(istream));
 
-		String receiveMessage, sendMessage;               
-		
+		String receiveMessage;
+		String sendMessage;
+
 		/*
 		 * Keeps connection active by checking constantly if the client sent anything to the server
 		 * and can send messages back.
 		 */
-		while(true)
-		{
-			if((receiveMessage = receiveRead.readLine()) != null)  
-			{
-				System.out.println(receiveMessage);         
-			}         
-			sendMessage = keyRead.readLine(); 
-			pwrite.println(sendMessage);             
-			pwrite.flush(); //Flush data from memory
-		}               
+		while (true) {
+			if((receiveMessage = receiveRead.readLine()) != null) {
+				sendMessage = processInput(receiveMessage);
+				pwrite.println(sendMessage);
+				pwrite.flush(); //Flush data from memory
+			}
+			if (receiveMessage.equals("exit")) break;
+		}
+	}
+
+	private static String processInput(String input) {
+		return input;
 	}
 }
